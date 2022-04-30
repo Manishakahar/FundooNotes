029363191e4ddb4d26ca55862a88056e8522aa1f
Manisha <manisha.kahar1998@gmail.com>
2022-04-30 10:59:19 +0530

    Added: Added Authguard For login Permission Of fundoo Notes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,41 +5,28 @@ import { RegistrationComponent } from './component/registration/registration.com
 import { ForgetPaswordComponent } from './component/forget-passwored/forget-passwored.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { CreateNoteComponent } from './component/create-note/create-note.component';
-import { DisplaynoteComponent } from './component/displaynote/displaynote.component'; 
+import { DisplaynoteComponent } from './component/displaynote/displaynote.component';
 import { GetAllNotesComponent } from './component/get-all-notes/get-all-notes.component';
 import { UpdatenoteComponent } from './component/updatenote/updatenote.component';
 import { TrashComponent } from './component/trash/trash.component';
 import { ArchiveComponent } from './component/archive/archive.component';
-// import {AuthenticationGuard} from './authentication.guard';  
-
-
+import { AuthguardnameGuard } from './authguardname.guard';
 const routes: Routes = [
   { path: 'register', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'forget', component: ForgetPaswordComponent },
-   { path: 'update', component: UpdatenoteComponent},
-  //  {path:'home',component:DashboardComponent,canActivate:[AuthenticationGuard]},  
-  { path: 'dashboard', component: DashboardComponent,
-  
-  children: [
-    {
-      path: '',
-      redirectTo: 'notes',
-      pathMatch: 'full',
-    },
-    {
-      path: "notes",
-      component: GetAllNotesComponent,
-    },
-    {
-      path: "trash",
-      component: TrashComponent,
-    },
-    {
-      path: "archive",
-      component: ArchiveComponent,
-    },
-  ]},
+  { path: 'update', component: UpdatenoteComponent },
+  // { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'dashboard', component: DashboardComponent, canActivate: [AuthguardnameGuard],
+
+    children: [
+      { path: '', redirectTo: 'notes', pathMatch: 'full', },
+      { path: "notes", component: GetAllNotesComponent, },
+      { path: "trash", component: TrashComponent },
+      { path: "archive", component: ArchiveComponent},
+    ]
+  },
 ];
 
 @NgModule({
